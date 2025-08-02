11ac456da76d3693a621a23e45fda3d518d40f88
andresjee <andresjee>
2025-08-02 11:56:42 -0500

    subiendo archivo

---
+++ prueba.js
@@ -0,0 +1,10 @@
+var numero="8";
+var sueldo="5.000.000";
+var edad ="35";
+var hijos="2";
+let nombre="jhon andres espinosa";
+let femenino=false
+let masculino=true
+
+console.log("el valor del numero " +numero + " su nomre es "+ nombre + " con una edad de "+edad + " con un sueldo de "+ sueldo+" cantidad de hijos que desea tener "+hijos);
+console.log(" mi genero es " + " femenino " + femenino+ " MASCULINO "+masculino)
